d014245e8a95b13c4462b0ced0857e92a7257822
Talha <talhakamalia@gmail.com>
2017-05-18 19:02:22 +0500

    goutte

--- config/app.php
+++ config/app.php
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Weidner\Goutte\GoutteServiceProvider::class, // [1]
 
         /*
          * Application Service Providers...
@@ -225,6 +224,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Goutte' => Weidner\Goutte\GoutteFacade::class, // [2]
 
     ],
 
